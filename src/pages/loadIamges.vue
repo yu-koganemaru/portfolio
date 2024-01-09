225133b69bd1791881d97b80963a3b8a9bf05b59
小金丸友 <koganemaruyu@koganemarutomonoMacBook-Pro.local>
2024-01-09 09:32:20 +0900

    fix yml

---
+++ src/pages/loadIamges.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+img(src="~/assets/images/thumbnail-booklink.png")
+img(src="~/assets/images/thumbnail-nursecall.png")
+img(src="~/assets/images/thumbnail-portfolio.png")
+img(src="~/assets/images/thumbnail-swk.png")
+</template>
